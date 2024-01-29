9a3b50a21b15b6291b77687a25b15d26ebd588cb
Alexandra <alexa_luz@mail.ru>
2024-01-29 14:29:43 +0300

    first solution

--- Program.cs
+++ Program.cs
@@ -2,3 +2,19 @@
 //Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 //Использовать рекурсию, не использовать циклы.
 
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void Even(int m, int n)
+{
+if (m > n)
+return;
+if (m % 2 == 0)
+{
+Console.Write($"{m}, ");
+}
+Even(m+1,n);
+
+}
+Even(m,n);
