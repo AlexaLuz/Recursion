3d8b478510ca32744330fa67e08ab5db68f916b1
Alexandra <alexa_luz@mail.ru>
2024-01-29 14:55:08 +0300

    second solution

--- Program.cs
+++ Program.cs
@@ -2,19 +2,38 @@
 //Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 //Использовать рекурсию, не использовать циклы.
 
+//Console.Write("Введите M: ");
+//int m = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите N: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+//void Even(int m, int n)
+//{
+//if (m > n)
+//return;
+//if (m % 2 == 0)
+//{
+//Console.Write($"{m}, ");
+//}
+//Even(m+1,n);
+
+//}
+//Even(m,n);
+
+
+
+//Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
 Console.Write("Введите M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-void Even(int m, int n)
-{
-if (m > n)
-return;
-if (m % 2 == 0)
-{
-Console.Write($"{m}, ");
-}
-Even(m+1,n);
 
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m - 1, 1);
+else return akkerman(m - 1, akkerman(m, n - 1));
 }
-Even(m,n);
+
+Console.Write($"{akkerman(m, n)} ");
