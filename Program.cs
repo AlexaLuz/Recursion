8367227cfffae4b3da774367f31757c443415d09
Alexandra <alexa_luz@mail.ru>
2024-02-01 15:54:23 +0300

    third solution

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 //Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 //Использовать рекурсию, не использовать циклы.
 
-//Console.Write("Введите M: ");
+//Console.Write("Введите m: ");
 //int m = Convert.ToInt32(Console.ReadLine());
-//Console.Write("Введите N: ");
+//Console.Write("Введите n: ");
 //int n = Convert.ToInt32(Console.ReadLine());
 //void Even(int m, int n)
 //{
@@ -24,16 +24,44 @@
 //Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 //Даны два неотрицательных числа m и n.
 
-Console.Write("Введите M: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите N: ");
-int n = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите m: ");
+//int m = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите n: ");
+//int n = Convert.ToInt32(Console.ReadLine());
 
-int akkerman(int m, int n)
+//int akkerman(int m, int n)
+//{
+//if (m == 0) return n + 1;
+//else if (n == 0) return akkerman(m - 1, 1);
+//else return akkerman(m - 1, akkerman(m, n - 1));
+//}
+
+//Console.Write($"{akkerman(m, n)} ");
+
+
+
+//Задайте произвольный массив. Выведете его элементы, начиная с конца.
+//Использовать рекурсию, не использовать циклы.
+
+ int Rev(int array[], int len) 
 {
-if (m == 0) return n + 1;
-else if (n == 0) return akkerman(m - 1, 1);
-else return akkerman(m - 1, akkerman(m, n - 1));
+  if (len < 0) 
+  {
+return 0;
+  } 
+  else
+  {
+    array[len];
+    cout = array[len];
+    Rev(array, len-1);
+  }
 }
+    
+int main() 
+{
+  int arr[6] = { new arr [56, 8, 15, 17, 26, 27]};
 
-Console.Write($"{akkerman(m, n)} ");
+Rev(arr, 5);
+
+  return 0;
+}
